1c09cf4a236ff333590d23ae181cbf3f49354cdd
Alexis Luna <mceclipse1889@gmail.com>
2025-10-21 14:00:30 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,25 +10,25 @@ MENU.addEventListener("click", toggleOverlay);
 CLOSE_BTN.addEventListener("click", toggleOverlay);
 
 
-
+/* Gallary Horizontal Scrolling */
 const CONTAINER = document.querySelector("#our-work-container");
 const TRACK = document.querySelector("#image-scroll-track");
 
-    if (CONTAINER && TRACK) {
-        function handleHorizontalScroll() {
-            const CONTAINER_TOP = CONTAINER.offsetTop;
-            const CONTAINER_HEIGHT = CONTAINER.offsetHeight;
-            const VIEWPORT_HEIGHT = window.innerHeight;
-            const SCROLL_TOP = window.scrollY;
-            const SCROLLABLE_DISTANCE = CONTAINER_HEIGHT - VIEWPORT_HEIGHT;
-            const SCROLL_OFFSET = SCROLL_TOP - CONTAINER_TOP; 
-            const PROGRESS = Math.min(1, Math.max(0, SCROLL_OFFSET / (SCROLLABLE_DISTANCE * 0.8)));
-            const MAX_SCROLL = TRACK.scrollWidth - window.innerWidth;
-            const TRANSFORM_X = PROGRESS * MAX_SCROLL;
+if (CONTAINER && TRACK) {
+    function handleHorizontalScroll() {
+        const CONTAINER_TOP = CONTAINER.offsetTop;
+        const CONTAINER_HEIGHT = CONTAINER.offsetHeight;
+        const VIEWPORT_HEIGHT = window.innerHeight;
+        const SCROLL_TOP = window.scrollY;
+        const SCROLLABLE_DISTANCE = CONTAINER_HEIGHT - VIEWPORT_HEIGHT;
+        const SCROLL_OFFSET = SCROLL_TOP - CONTAINER_TOP; 
+        const PROGRESS = Math.min(1, Math.max(0, SCROLL_OFFSET / (SCROLLABLE_DISTANCE * 1)));
+        const MAX_SCROLL = TRACK.scrollWidth - window.innerWidth;
+        const TRANSFORM_X = PROGRESS * MAX_SCROLL;
 
-            TRACK.style.transform = `translateX(-${TRANSFORM_X}px)`;
-        }
-
-        window.addEventListener("scroll", handleHorizontalScroll);
-        handleHorizontalScroll();
+        TRACK.style.transform = `translateX(-${TRANSFORM_X}px)`;
     }
+
+    window.addEventListener("scroll", handleHorizontalScroll);
+    handleHorizontalScroll();
+}

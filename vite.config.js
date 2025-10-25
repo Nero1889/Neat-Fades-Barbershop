68070243f1a2c0607c703fae073815ded0818d85
Alexis Luna <mceclipse1889@gmail.com>
2025-10-24 23:47:35 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import {resolve} from 'path';
 import {defineConfig} from "vite"
 import tailwindcss from "@tailwindcss/vite"
 
@@ -5,4 +6,14 @@ export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                services: resolve(__dirname, 'services.html'),
+                barbers: resolve(__dirname, 'barbers.html'),
+                hours: resolve(__dirname, 'hours.html')
+            }
+        }
+    }
 })
